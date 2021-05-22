7460c32d835cf4944d6bb866b3acdf47b99e6f86
unknown <mdziubin@ualberta.ca>
2021-05-22 16:17:12 -0600

    simplified App.js layout

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
 import React from "react";
-import { Grid } from "@material-ui/core";
 
 import Main from "./layouts/Main";
 
 function App() {
-  return (
-    <Grid container direction="column">
-      <Grid item container>
-        <Grid item xs={false} sm={1} />
-        <Grid item xs={12} sm={10}>
-          <Main />
-        </Grid>
-        <Grid item xs={false} sm={1} />
-      </Grid>
-    </Grid>
-  );
+  return <Main />;
 }
 
 export default App;
