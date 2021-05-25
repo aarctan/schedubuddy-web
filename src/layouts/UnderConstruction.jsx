672a21be6214eca6155c0b4e0b82965a38f5175b
Exanut <nihilch@protonmail.com>
2021-05-25 13:36:13 -0400

    under construction bar for prod

---
+++ src/layouts/UnderConstruction.jsx
@@ -0,0 +1,31 @@
+import { AppBar, Toolbar, Typography, makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  root: {
+    flexGrow: 1,
+  },
+  appbar: {
+    position: "absolute",
+    color: "secondary",
+    alignItems: "center",
+  },
+  typography: {
+    flexGrow: 1,
+    textAlign: "center",
+  },
+});
+
+export default function Footer() {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <AppBar color="secondary">
+        <Toolbar>
+          <Typography color="inherit" variant="h5" className={classes.typography}>
+            Webapp under construction
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
