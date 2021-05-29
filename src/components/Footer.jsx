3dcfeca51edc0aa8172413d4e22f722b7cdc58b1
unknown <mdziubin@ualberta.ca>
2021-05-29 13:51:18 -0600

    Revert "margintop to footer and add text"
    This reverts commit 0908eea6c0ef41cd7383a0ed4b6b65dc49ae67c3.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,23 +1,40 @@
 import { makeStyles } from "@material-ui/core/styles";
 import { Container, Typography } from "@material-ui/core";
 
+const Copyright = () => {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      Copyright
+    </Typography>
+  );
+};
+
 const useStyles = makeStyles((theme) => ({
   footer: {
     backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(0.5, 0),
-    marginTop: "5%",
+    padding: theme.spacing(2, 0),
   },
 }));
 
 const Footer = (props) => {
   const classes = useStyles();
+  const { description, title } = props;
 
   return (
     <footer className={classes.footer}>
       <Container maxWidth="lg">
-        <Typography variant="body2" color="textSecondary" align="center">
-          Created by Muhammad Usman and Matthew Dziubina
+        <Typography variant="h6" align="center" gutterBottom>
+          {title}
         </Typography>
+        <Typography
+          variant="subtitle1"
+          align="center"
+          color="textSecondary"
+          component="p"
+        >
+          {description}
+        </Typography>
+        <Copyright />
       </Container>
     </footer>
   );
