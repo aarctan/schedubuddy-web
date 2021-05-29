0908eea6c0ef41cd7383a0ed4b6b65dc49ae67c3
Exanut <nihilch@protonmail.com>
2021-05-29 15:48:45 -0400

    margintop to footer and add text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,40 +1,23 @@
 import { makeStyles } from "@material-ui/core/styles";
 import { Container, Typography } from "@material-ui/core";
 
-const Copyright = () => {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      Copyright
-    </Typography>
-  );
-};
-
 const useStyles = makeStyles((theme) => ({
   footer: {
     backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(2, 0),
+    padding: theme.spacing(0.5, 0),
+    marginTop: "5%",
   },
 }));
 
 const Footer = (props) => {
   const classes = useStyles();
-  const { description, title } = props;
 
   return (
     <footer className={classes.footer}>
       <Container maxWidth="lg">
-        <Typography variant="h6" align="center" gutterBottom>
-          {title}
+        <Typography variant="body2" color="textSecondary" align="center">
+          Created by Muhammad Usman and Matthew Dziubina
         </Typography>
-        <Typography
-          variant="subtitle1"
-          align="center"
-          color="textSecondary"
-          component="p"
-        >
-          {description}
-        </Typography>
-        <Copyright />
       </Container>
     </footer>
   );
