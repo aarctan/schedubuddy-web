058e53447176dfc174064ac0554dd689b358fb92
Exanut <nihilch@protonmail.com>
2021-07-29 19:52:56 -0400

    auto resize pagination component

--- src/components/Paging.jsx
+++ src/components/Paging.jsx
@@ -1,13 +1,16 @@
 import Pagination from "@material-ui/core/Pagination";
 import Stack from "@material-ui/core/Stack";
+import { useMediaQuery } from "@material-ui/core";
 
 export default function Paging(props) {
+  const isSmallScreen = useMediaQuery((theme) => theme.breakpoints.down("sm"));
   return (
     <Stack spacing={2}>
       <Pagination
         style={{ marginBottom: "2%" }}
         variant="outlined"
         color="primary"
+        size={isSmallScreen ? "small" : "medium"}
         count={props.pages}
         onChange={props.onChange}
         showFirstButton
