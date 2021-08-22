92a00bafaf06eb132f2c0e52fb72b082e769aacd
mdziubin <mdziubin@ualberta.ca>
2021-08-21 18:58:06 -0600

    Refactored MiniSelect to use grids

--- src/components/Input/MiniSelect.jsx
+++ src/components/Input/MiniSelect.jsx
@@ -1,63 +1,26 @@
-import { InputLabel, makeStyles, useMediaQuery } from "@material-ui/core";
+import { Grid, InputLabel, makeStyles } from "@material-ui/core";
 import BasicSelect from "./BasicSelect";
 
-const useWideStyles = makeStyles((theme) => ({
-  root: {
-    display: "flex",
-    justifyContent: "space-between",
-  },
-  label: {
-    alignItems: "center",
-    color: theme.palette.text.primary,
-    display: "flex",
-    whiteSpace: "normal",
-    width: "60%",
-  },
-  input: {
-    width: "35%",
-  },
-}));
-
-// Display label to the left of the input rather than embbed(default)
-const InlineSelect = ({ label, ...rest }) => {
-  const classes = useWideStyles();
-
-  return (
-    <div className={classes.root}>
-      <InputLabel className={classes.label}>{label}</InputLabel>
-      <BasicSelect className={classes.input} {...rest} />
-    </div>
-  );
-};
-
-const useCompactStyles = makeStyles((theme) => ({
-  root: {
-    display: "flex",
-    flexDirection: "column",
-  },
+const useStyles = makeStyles((theme) => ({
   label: {
     color: theme.palette.text.primary,
     whiteSpace: "normal",
   },
 }));
 
-// Display label above the input rather than embbed(default)
-const CompactSelect = ({ label, ...rest }) => {
-  const classes = useCompactStyles();
+const MiniSelect = ({ label, ...rest }) => {
+  const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <InputLabel className={classes.label}>{label}</InputLabel>
-      <BasicSelect {...rest} />
-    </div>
+    <Grid container direction="row" justifyContent="space-between" alignItems="center">
+      <Grid item xs={12} lg={6}>
+        <InputLabel className={classes.label}>{label}</InputLabel>
+      </Grid>
+      <Grid item xs={12} lg={4}>
+        <BasicSelect {...rest} />
+      </Grid>
+    </Grid>
   );
-};
-
-const MiniSelect = (props) => {
-  const small = useMediaQuery((theme) => theme.breakpoints.up("lg"));
-
-  // Display different versions the select input depending on screen width
-  return small ? <InlineSelect {...props} /> : <CompactSelect {...props} />;
 };
 
 export default MiniSelect;
