e4be8bd63c9b245dbd3df006426cfb90c0bdd04b
Exanut <nihilch@protonmail.com>
2022-02-05 17:04:59 -0700

    remove announcement header

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -28,8 +28,7 @@ const Main = () => {
   }, []);
 
   return (
-    <Box mt={10}>
-      <UnderConstruction />
+    <Box mt={3}>
       <Grid container spacing={2}>
         <Grid item xs={12} md={4}>
           <ControlContainer
