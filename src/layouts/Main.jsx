1e071f70e0a1a501863ce938df8702617be3e747
Exanut <nihilch@protonmail.com>
2022-07-03 19:19:33 -0700

    remove banner

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -29,40 +29,37 @@ const Main = () => {
 
   return (
     <>
-    <div>
-      <UnderConstruction/>
-    </div>
-    <Box mt={10}>
-      <Grid container spacing={2}>
-        <Grid item xs={12} md={4}>
-          <ControlContainer
-            setCourseOrder={setCourseOrder}
-            setSchedules={setSchedules}
-            setAliases={setAliases}
-            showInstructorPref={showInstructorPref}
-            setShowInstructorPref={setShowInstructorPref}
-            setLoading={setLoading}
-            setErrmsg={setErrmsg}
-            loading={loading}
-            terms={terms}
+      <Box mt={3}>
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={4}>
+            <ControlContainer
+              setCourseOrder={setCourseOrder}
+              setSchedules={setSchedules}
+              setAliases={setAliases}
+              showInstructorPref={showInstructorPref}
+              setShowInstructorPref={setShowInstructorPref}
+              setLoading={setLoading}
+              setErrmsg={setErrmsg}
+              loading={loading}
+              terms={terms}
             />
-        </Grid>
-        <Grid item xs={12} md={8}>
-          {loading ? (
-            <LoadingCard />
+          </Grid>
+          <Grid item xs={12} md={8}>
+            {loading ? (
+              <LoadingCard />
             ) : (
               <ScheduleContainer
-              courseOrder={courseOrder}
-              schedules={schedules}
-              aliases={aliases}
-              showInstructorPref={showInstructorPref}
-              errmsg={errmsg}
+                courseOrder={courseOrder}
+                schedules={schedules}
+                aliases={aliases}
+                showInstructorPref={showInstructorPref}
+                errmsg={errmsg}
               />
-              )}
+            )}
+          </Grid>
         </Grid>
-      </Grid>
-    </Box>
-              </>
+      </Box>
+    </>
   );
 };
 
