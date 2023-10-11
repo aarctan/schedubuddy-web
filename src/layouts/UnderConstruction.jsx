84717d7d20554ccc807c36bf9a2b70408ee93a10
Muhammad Usman <nihilch@protonmail.com>
2023-10-11 15:35:15 -0700

    late update warning

--- src/layouts/UnderConstruction.jsx
+++ src/layouts/UnderConstruction.jsx
@@ -4,13 +4,13 @@ export default function Footer() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar color="secondary">
-        <Toolbar>
+        <Toolbar variant="dense">
           <Typography
             color="inherit"
             variant="h6"
             sx={{ flexGrow: 1, textAlign: "center" }}
           >
-            The catalog recently updated - some classes may currently be missing here
+            The catalog recently updated - some class times or locations may have changed
           </Typography>
         </Toolbar>
       </AppBar>
