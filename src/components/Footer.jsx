e166d45f60caedc767563922f82042afabf277bc
Muhammad Usman <nihilch@protonmail.com>
2023-10-27 00:00:09 -0700

    update last updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,7 +13,7 @@ const UnstyledFooter = ({ className }) => {
         <GitHubIcon fontSize="large" />
       </IconButton>
       <Typography variant="caption" color="secondary">
-        <div>Last updated Oct 21</div>
+        <div>Last updated Oct 27</div>
       </Typography>
     </footer>
   );
