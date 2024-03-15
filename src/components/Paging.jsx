b85b5959d26743f991711f80189506a51818e361
mdziubin <mdziubin@ualberta.ca>
2024-03-14 20:31:18 -0600

    Add margin to pagination

--- src/components/Paging.jsx
+++ src/components/Paging.jsx
@@ -4,18 +4,16 @@ export default function Paging(props) {
   const isSmallScreen = useMediaQuery((theme) => theme.breakpoints.down("sm"));
 
   return (
-    <Stack spacing={2}>
-      <Pagination
-        sx={{ marginBottom: "2%" }}
-        variant="outlined"
-        color="primary"
-        size={isSmallScreen ? "small" : "medium"}
-        siblingCount={isSmallScreen ? 0 : 1}
-        count={props.pages}
-        onChange={props.onChange}
-        showFirstButton
-        showLastButton
-      />
-    </Stack>
+    <Pagination
+      sx={{ marginBottom: 1 }}
+      variant="outlined"
+      color="primary"
+      size={isSmallScreen ? "small" : "medium"}
+      siblingCount={isSmallScreen ? 0 : 1}
+      count={props.pages}
+      onChange={props.onChange}
+      showFirstButton
+      showLastButton
+    />
   );
 }
